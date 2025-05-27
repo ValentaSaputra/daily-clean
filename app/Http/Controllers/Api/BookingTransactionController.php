e1bcfd5b255831175a930ea80fd5e287f877f6f8
valentasaputra <valentaofficial@gmail.com>
2025-05-27 22:24:45 +0700

    Implement BookingTransaction creation and validation, add Category and HomeService API resources, and update API routes

--- app/Http/Controllers/Api/BookingTransactionController.php
+++ app/Http/Controllers/Api/BookingTransactionController.php
@@ -3,9 +3,102 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreBookingTransactionRequest;
+use App\Http\Resources\Api\BookingTransactionApiResource;
+use App\Models\BookingTransaction;
+use App\Models\HomeService;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 
 class BookingTransactionController extends Controller
 {
     //
+    public function store(StoreBookingTransactionRequest $request)
+    {
+        try {
+            // Validate the request data
+            $validatedData = $request->validated();
+
+            // Handle the file upload
+            if ($request->hasFile('proof')) {
+                $filePath = $request->file('proof')->store('proofs', 'public');
+                $validatedData['proof'] = $filePath;
+            }
+
+            // Retrive the service ID from the request
+            $serviceId = $request->input('service_ids'); // no need to decord json, as it is already an array
+
+            if (empty($serviceIds)) {
+                return response()->json(['message' => 'No services selected'], 400);
+            }
+
+            // fetch services from the database
+            $services = HomeService::whereIn('id', $serviceIds)->get();
+            if ($services->isEmpty()) {
+                return response()->json(['message' => 'Invalid services'], 404);
+            }
+
+            // calculate the total price, tax, insurance, and grand total
+            $totalPrice = $services->sum('price');
+            $tax = $totalPrice * 0.12;
+            $grandTotal = $totalPrice + $tax;
+
+            // use carbon to set schedule_at to tommorow's date
+            // agar safety saat dari frontend menginputkan waktu yang sudah lewat / tahun lalu
+            $validatedData['schedule_at'] = Carbon::tommorow()->toDateString();
+
+            // populate the booking transaction data
+            $validatedData['total_price'] = $grandTotal;
+            $validatedData['total_tax_amount'] = $tax;
+            $validatedData['sub_total'] = $totalPrice;
+            $validatedData['is_paid'] = false; // default to false, can be updated later
+            $validatedData['booking_trx_id'] = BookingTransaction::generateUniqueTrxId();
+
+            // Create the booking transaction
+            $bookingTransaction = BookingTransaction::create($validatedData);
+
+            if (!$bookingTransaction) {
+                return response()->json(['message' => 'Booking Transaction not created'], 500);
+            }
+
+            // Create transaction details for each service
+            foreach ($services as $service) {
+                $bookingTransaction->transactionDetails()->create([
+                    'home_service_id' => $service->id,
+                    'price' => $service->price,
+                ]);
+            }
+
+            // Return the created booking transaction with details
+            // load untuk menampilkan relasi transactionDetails
+            return new BookingTransactionApiResource($bookingTransaction->load('transactionDetails'));
+
+            // 
+        } catch (\Exception $e) {
+            return response()->json(['message' => 'An error occurred', 'error' => $e->getMessage()], 500);
+        }
+    }
+
+
+    // 
+    public function booking_details(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|string',
+            'booking_trx_id' => 'required|string',
+        ]);
+
+        $booking = BookingTransaction::where('email', $request->email)
+            ->where('booking_trx_id', $request->booking_trx_id)
+            ->with([
+                'transactionDetails',
+                'transactionDetails.homeService',
+            ])
+            ->first();
+
+        if (!$booking) {
+            return response()->json(['message' => 'Booking not found'], 404);
+        }
+        return new BookingTransactionApiResource($booking);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\Api\BookingTransactionController;
+use App\Http\Controllers\Api\CategoryController;
+use App\Http\Controllers\Api\HomeServiceController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -15,5 +18,5 @@ Route::apiResource('/services', HomeServiceController::class);
 Route::get('category/{category:slug}', [CategoryController::class, 'show']);
 Route::apiResource('/categories', CategoryController::class);
 
-Route::post('/booking-transactions', [BookingtransactionController::class, 'store']);
+Route::post('/booking-transactions', [BookingTransactionController::class, 'store']);
 Route::post('/check-booking', [BookingTransactionController::class, 'booking_details']);
